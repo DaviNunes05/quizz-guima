682129cc2c2e19860eaff0155a51ec5d166858b1
Davi Nunes <davi.nunes05@gmail.com>
2023-12-27 10:04:47 -0300

    corrigindo validacao

--- repository/QuizRepository.js
+++ repository/QuizRepository.js
@@ -7,13 +7,20 @@ const buscarResultadosPorData = async (quizData) => {
 
 const gravarResultado = async (quizData, novoResultado) => {
 	const filtro = { data: quizData, "resultados.re": novoResultado.re };
+	const resultadoExistente = await quizModel.findOne(filtro);
+
+	if (resultadoExistente) {
+		// Se já existe um resultado com o mesmo 're', não faz nada e apenas retorna o documento atual
+		return resultadoExistente;
+	}
+
 	const update = {
 		$setOnInsert: { data: quizData },
-		$addToSet: { "resultados": novoResultado }
+		$addToSet: { resultados: novoResultado }
 	};
 	const options = { upsert: true, new: true, setDefaultsOnInsert: true };
 	const quizAtualizado = await quizModel.findOneAndUpdate(
-		filtro,
+		{ data: quizData },
 		update,
 		options
 	);
